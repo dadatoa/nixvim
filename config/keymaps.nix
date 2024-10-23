ebcb1a35759bc02efa8c49a9ac691c141d41e810
Dadatoa <f0a202@outlook.com>
2024-10-23 18:21:27 +0700

    update navigation keymaps

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -1,3 +1,25 @@
 {
   globals.mapleader = " ";
+  keymaps = [
+    {
+      mode = ["n"];
+      key = "<c-k>";
+      action = ":wincmd k<CR>";
+    }
+    {
+      mode = ["n"];
+      key = "<c-j>";
+      action = ":wincmd j<CR>";
+    }
+    {
+      mode = ["n"];
+      key = "<c-h>";
+      action = ":wincmd h<CR>";
+    }
+    {
+      mode = ["n"];
+      key = "<c-l>";
+      action = ":wincmd l<CR>";
+    }
+  ];
 }

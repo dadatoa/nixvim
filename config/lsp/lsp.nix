4ba0f6f4bd472a82ac60260f33e8e584c1a81696
Dadatoa <f0a202@outlook.com>
2025-05-26 16:54:24 +0700

    reconfig conform

--- config/lsp/lsp.nix
+++ config/lsp/lsp.nix
@@ -1,55 +1,34 @@
 {pkgs, ...}: {
   plugins = {
-    lsp-lines = {enable = true;};
-    lsp-format = {enable = true;};
-    helm = {enable = true;};
+    lsp-lines.enable = true;
+    lsp-format.enable = true;
     lsp = {
       enable = true;
       inlayHints = true;
       servers = {
         ansiblels = {enable = true;};
-        astro = {enable = true;};
-        gopls = {enable = true;};
-        helm_ls = {
+        astro.enable = true;
+        emmet_ls.enable = true;
+        gopls.enable = true;
+        html.enable = true;
+        jsonls.enable = true;
+        lua_ls.enable = true;
+        marksman.enable = true;
+        nil_ls.enable = true;
+
+        nixd = {
           enable = true;
-          extraOptions = {
-            settings = {
-              "helm-ls" = {
-                yamlls = {
-                  path = "${pkgs.yaml-language-server}/bin/yaml-language-server";
-                };
-              };
-            };
+          settings = {
+            formatting.command = ["nixpkgs-fmt"];
+            nixpkgs.expr = "import <nixpkgs> {}";
           };
         };
-        html = {enable = true;};
-        jsonls = {enable = true;};
-        lua_ls = {enable = true;};
-        marksman = {enable = true;};
-        nil_ls = {enable = true;};
-        pyright = {enable = true;};
-        terraformls = {enable = true;};
-        ts_ls = {enable = true;};
+
+        pyright.enable = true;
+        terraformls.enable = true;
+        ts_ls.enable = true;
         yamlls = {
           enable = true;
-          extraOptions = {
-            settings = {
-              yaml = {
-                schemas = {
-                  kubernetes = "'*.yaml";
-                  "http://json.schemastore.org/github-workflow" = ".github/workflows/*";
-                  "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
-                  "http://json.schemastore.org/ansible-stable-2.9" = "roles/tasks/*.{yml,yaml}";
-                  "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
-                  "http://json.schemastore.org/ansible-playbook" = "*play*.{yml,yaml}";
-                  "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
-                  "https://json.schemastore.org/dependabot-v2" = ".github/dependabot.{yml,yaml}";
-                  "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" = "*docker-compose*.{yml,yaml}";
-                  "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" = "*flow*.{yml,yaml}";
-                };
-              };
-            };
-          };
         };
       };
 
